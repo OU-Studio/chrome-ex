4205a6b8a47d1a6a1b78de5f6f3f5ad403828acf
Joe <joe@ou.studio>
2025-08-08 22:24:33 +0100

    a

--- background.js
+++ background.js
@@ -8,3 +8,17 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     });
   }
 });
+
+// Reset all tool toggles whenever a tab starts loading
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (changeInfo.status === 'loading' && tab.url && tab.url.includes('.squarespace.com')) {
+    chrome.storage.local.set({
+      inspectorEnabled: false,
+      altCheckerEnabled: false,
+      styleSyncEnabled: false,
+      snippetEnabled: false, // if you add a toggle for this later
+    });
+    // Optionally tell the page to detach tools (usually not needed on reload)
+    chrome.tabs.sendMessage(tabId, { type: 'OU_RESET_TOOLS' }).catch(() => {});
+  }
+});
